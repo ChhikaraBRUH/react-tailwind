af56b4dc6f56ef9d21945a073d141d579ec18d85
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2023-12-28 12:47:40 +0530

    style: basic page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div>
+    <div className="flex items-center justify-center flex-col">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
